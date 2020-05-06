d85d3dd8d238b7142e7cf0a0a39ba97af3ca824b
Alexei Abianna <alexeiabianna@gmail.com>
2020-05-06 01:08:43 -0300

    Fix data output

--- sateliteOrbita.py
+++ sateliteOrbita.py
@@ -1,15 +1,19 @@
+import math
 #
 # Cálculo da altura (h) para que um satélite atinga uma órbita geoestacionária.
 #
 
 T = float(input("Digite o tempo de órbita: "))
+T = T * 60
 
-G = 6.67428 * 1e-11
-M = 5.97 * 1e24
-R = 6371 * 1000
-pi = 3.14159
+G = 6.67428e-11
+M = 5.97e24
+R = 6371e3
+pi = math.pi
 
-h = (((G * M * (T**2))/(4 * pi)**2)**(1/3)) - R
+h = ((G * M * T**2) / (4 * pi ** 2)) ** (1/3) - R
+
+
 
 
 print(R)
