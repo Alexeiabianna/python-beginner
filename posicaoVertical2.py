55200ec2b5383344ba418e0426fb328b4beb37d2
Alexei Abianna <alexeiabianna@gmail.com>
2020-05-07 11:27:58 -0300

    Modify include conditions

--- posicaoVertical2.py
+++ posicaoVertical2.py
@@ -4,10 +4,29 @@ import math
 #
 
 vi = float(input("Digite a velocidade inicial da bola: "))
+y = float(input("Digite a posição desejada: "))
 
 g = 9.81
-y = 0
-t = math.sqrt(vi * y * g * 2) # Falta resolver isso para quinta
+
+a = g / 2
+b = - vi
+c = y
+
+delta = b * b - 4 * a * c
+
+if delta >= 0:
+    if delta == 0:
+        x1 = (-b + math.sqrt(delta)) / (2 * a)
+        print("Apenas no tempo:", x1)
+
+    else:
+        x1 = (-b + math.sqrt(delta)) / (2 * a)
+        x2 = (-b - math.sqrt(delta)) / (2 * a)
+        print("tempos: ", x2, x1)
+
+else:
+    print("Não atinge esta altura")
+print("Fim")
 
 
-print("O tempo para a bola tocar o solo é de: ", t, "m")
+
